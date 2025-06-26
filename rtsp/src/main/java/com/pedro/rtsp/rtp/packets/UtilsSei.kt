a7f8132d66ef61963265a8197c502ebf5f36a58b
Yin Shen <yin.shen@siemens.com>
2025-06-26 13:36:25 +0800

    fix

--- rtsp/src/main/java/com/pedro/rtsp/rtp/packets/UtilsSei.kt
+++ rtsp/src/main/java/com/pedro/rtsp/rtp/packets/UtilsSei.kt
@@ -6,6 +6,7 @@ class UtilsSei {
     companion object {
         public fun muxSEI(msg: String): ByteArray {
             val seiContent = msg.toByteArray()
+            val naluHeader = byteArrayOf(0x00, 0x00, 0x00, 0x01)
             val seiType = byteArrayOf(0x06, 0x05)
             val seiUuid = byteArrayOf(
                 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04,
@@ -35,7 +36,7 @@ class UtilsSei {
             }
             val sei_content_size = combineArrays(contentFirstSize, contentLastSize)
 
-            return combineArrays(seiType, sei_content_size, seiUuid, seiContent, seiEnd)
+            return combineArrays(naluHeader, seiType, sei_content_size, seiUuid, seiContent, seiEnd)
         }
 
 //        private fun convertIntToByteArray(data: Int) : ByteArray {
